e23a26d7154f3e971f0b6fa5bc70f6738c84628a
Pignaut <cltrobins@gmail.com>
2017-05-14 01:53:21 +0100

    Fixed the star chart spawning problem

--- Assets/newScript.cs
+++ Assets/newScript.cs
@@ -4,57 +4,30 @@ using UnityEngine;
 
 public class newScript: MonoBehaviour {
 
-	int width, height;
 	int rand;
-
-	public GameObject plane;
 	public GameObject stars;
-
-	Bounds bounds;
-	public MeshRenderer rend;
-
-
 
 	// Use this for initialization
 	void Start ()
 	{
-		plane = GameObject.Find ("plane");
+		rand = Random.Range (5, 14);
 
-		rend = plane.GetComponent<MeshRenderer>();
-
-		rand = Random.Range (0, 9);
+		Debug.Log ("rand: ");
+		Debug.Log (rand);
 
 		GenerateMap ();
 	}
 
-	// Update is called once per frame
-	void Update ()
-	{
-		
-	}
-
-
 	void GenerateMap()
 	{
-		//int i = 0;
-			
-		//while(i < rand)
 		for(int i = 0; i < rand; i++)
 		{
-			Vector2 X = new Vector2 (rend.bounds.min.x, rend.bounds.max.x);
-			Vector2 Y = new Vector2(rend.bounds.min.y, rend.bounds.max.y);
+			float posX = Random.Range(-126, 124);
+			float posY = Random.Range(-126, 124);
 
-			float xAxisMin = X.x;
-			float xAxisMax = X.y;
-			float yAxisMin = X.x;
-			float yAxisMax = X.y;
-
-			float r1 = Random.Range(xAxisMin, xAxisMax);
-			float r2 = Random.Range(yAxisMin, yAxisMax);
+			stars.transform.position = new Vector3 (posX, 0.1f, posY);
 
 			Instantiate (stars);
-
-			i++;
 		}
 	}
 }
